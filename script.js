d9fdb8d4347ae2492d8dbaa5e44aaa054bf24a1e
project_1 <example@gmail.com>
2023-07-02 17:08:45 +0500

    add personalMovieDB

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const numberOfFlims = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFlims,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+};
